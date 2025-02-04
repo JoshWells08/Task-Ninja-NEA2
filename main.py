894919fdce2f84a8cb1568b3744a1340061249e0
JoshWells08 <19mwellsj@hbk.acesmat.uk>
2025-02-04 10:53:37 +0000

    Early GUI development

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import sys, time
+
+from pynput.mouse import Controller, Listener
+from pynput.keyboard import Controller, Listener, Key, KeyCode
+
 from PyQt5 import QtWidgets, QtCore, QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, \
     QLabel, QTextBrowser, QComboBox, QListWidget, QProgressBar, QMessageBox, QDialogButtonBox, \
     QScrollBar, QScrollArea
 from PyQt5 import uic
 from datetime import datetime
+
+recording = False
 
 class UI(QMainWindow):
 
@@ -37,43 +43,64 @@ class UI(QMainWindow):
     '''define events '''
 
     def CNMB_clicked(self):
-       uic.loadUi('CreateNewMacro.ui', self)
+         uic.loadUi('CreateNewMacro.ui', self)
 
-       '''initialise widgets within program'''
-       self.UserLogArea = self.findChild(QScrollArea, 'UserLogArea')
-       self.UserLog = self.findChild(QLabel, 'UserLog')
-       self.UserLogScrollBar = self.findChild(QScrollBar,'UserLogScrollBar')
+         '''initialise widgets within program'''
+         self.UserLogArea = self.findChild(QScrollArea, 'UserLogArea')
+         self.UserLog = self.findChild(QLabel, 'UserLog')
+         self.UserLogScrollBar = self.findChild(QScrollBar,'UserLogScrollBar')
 
-       self.MacroSlot1 = self.findChild(QPushButton, 'MacroSlot1')
-       self.MacroSlot2 = self.findChild(QPushButton, 'MacroSlot2')
-       self.MacroSlot3 = self.findChild(QPushButton, 'MacroSlot3')
-       self.MacroSlot4 = self.findChild(QPushButton, 'MacroSlot4')
-       self.MacroSlot5 = self.findChild(QPushButton, 'MacroSlot5')
+         self.RecordingNotRecording = self.findChild(QLabel,'RecordingNotRecording')
+         self.StopRecordingButton = self.findChild(QPushButton,'StopRecordingButton')
+         self.StopRecordingButton.setHidden(True)
 
-       self.show() # display widgets 
 
-       '''set event handlers '''
+         if recording == True:
+            self.RecordingNotRecording.setText('recording')
+            self.StopRecordingButton.setHidden(False)
+         else:
+            self.RecordingNotRecording.setText('not recording')
 
-       self.MacroSlot1.clicked.connect(self.MacroSlot1_Clicked)
-       self.UserLogScrollBar.sliderMoved.connect(self.UserLogScrollBar_Scrolled)
+
+
+         self.MacroSlot1 = self.findChild(QPushButton, 'MacroSlot1')
+         self.MacroSlot2 = self.findChild(QPushButton, 'MacroSlot2')
+         self.MacroSlot3 = self.findChild(QPushButton, 'MacroSlot3')
+         self.MacroSlot4 = self.findChild(QPushButton, 'MacroSlot4')
+         self.MacroSlot5 = self.findChild(QPushButton, 'MacroSlot5')
+
+         self.show() # display widgets 
+
+         '''set event handlers '''
+
+         self.MacroSlot1.clicked.connect(self.MacroSlot1_Clicked)
+         self.UserLogScrollBar.sliderMoved.connect(self.UserLogScrollBar_Scrolled)
 
 
     def MacroSlot1_Clicked(self):
        print(self.UserLog.text())
        for i in range (50):
-        self.UserLog.setText(str(self.UserLog.text()) + 'MacroSlot1 Clicked! \n')
+        self.UserLog.setText(f'{str(self.UserLog.text())} MacroSlot1 Clicked!{str(i)}\n')
     
     def UserLogScrollBar_Scrolled(self):
        return True #placeholder value        
-    def REMB_clicked(self):
+    
+    def REMB_clicked(self): #run existing macro button
        return True #placeholder return value for valid parsing 
 
         
-    def OSB_clicked(self):
+    def OSB_clicked(self): #open settings button 
       return True #placeholder return value for valid parsing 
 
 
 app = QApplication(sys.argv)
 window = UI()
 app.exec_()
+
+
+
+
+
+
+
 sys.exit(app.exec_())
